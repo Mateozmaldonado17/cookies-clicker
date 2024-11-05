92a955a7c74fc0e9568e210cbd6dfa5ba00b33b6
Mateo Zapata <mszado15@gmail.com>
2024-11-05 17:30:05 -0500

    feat: finished 2 new interfaces

--- src/interfaces/account.interface.ts
+++ src/interfaces/account.interface.ts
@@ -1,11 +1,11 @@
-import IFactory from "./factory.interface";
+import IFactory from './factory.interface';
 
 interface IAccount {
   id: number;
   username: string;
   cookies: number;
   is_active: number;
-  factories: IFactory[]
+  factories: IFactory[];
 }
 
 export default IAccount;
