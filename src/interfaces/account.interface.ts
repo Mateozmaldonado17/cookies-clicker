85346d018c983bc71bd4672eadb40e3c2374e850
Mateo Zapata <mszado15@gmail.com>
2024-11-05 17:54:57 -0500

    feat: added icaccountfactory

--- src/interfaces/account.interface.ts
+++ src/interfaces/account.interface.ts
@@ -1,11 +1,13 @@
 import IFactory from './factory.interface';
 
+export type IAccountFactories = IFactory & { update_at: Date, level: number }
+
 interface IAccount {
   id: number;
   username: string;
   cookies: number;
   is_active: number;
-  factories: IFactory[];
+  factories: IAccountFactories[];
 }
 
 export default IAccount;
