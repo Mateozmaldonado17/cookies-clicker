fde53d224db12782cff070549afe64651a9ab23f
Mateo Zapata <mszado15@gmail.com>
2024-11-08 09:00:22 -0500

    feat: added service worker

---
+++ service-worker.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'media-cache-v2';
+const FILE_TYPES = [
+  '/index.html',
+  '.mp3',
+  '.png',
+  '.webp',
+  '.jpg',
+  '.css',
+  '.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache:', CACHE_NAME);
+      return cache.addAll(FILE_TYPES);
+    }),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (FILE_TYPES.some((type) => url.pathname.endsWith(type))) {
+    event.respondWith(
+      caches.match(request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(request)
+          .then((networkResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(request, networkResponse.clone());
+              return networkResponse;
+            });
+          })
+          .catch(() => {
+            if (request.destination === 'document') {
+              return caches.match('/index.html');
+            }
+          });
+      }),
+    );
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
